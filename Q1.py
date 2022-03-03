45df7ae98ac25abbeada08270009d97ef7f8c674
saumyaagrahari <90410699+saumyaagrahari@users.noreply.github.com>
2022-03-03 14:22:57 +0530

    Add files via upload

--- Q1.py
+++ Q1.py
@@ -73,3 +73,29 @@ while i<len(question_list):
             print("wrong ans")
             break
     i+=1 
+
+# a="anjali\nvaishali\n"
+# a+="rani\n"
+# print(a)
+
+# a="my name is anjali singh\n"
+# a+="i am student of navgurukul\n"
+# print(a)
+
+
+
+# i = 420
+# while i<=440:
+#     print(440-i)
+#     i+=1
+
+
+
+
+
+
+
+
+
+
+
